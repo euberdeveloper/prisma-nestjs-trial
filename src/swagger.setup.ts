f97d4792abd86180ac72e25ddafa3d5a7566c337
euberdeveloper <euberdeveloper@gmail.com>
2024-01-24 23:10:11 +0100

    feat: add swagger
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/swagger.setup.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+    const config = new DocumentBuilder()
+        .setTitle('NestJS Prisma Trial')
+        .setDescription('The NestJS-Prisma-Trial API description')
+        .setVersion('0.1')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+}
