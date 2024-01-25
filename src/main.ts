f1c5d7f0ed2d6a6ab5c0bb1f5f2646dfabbdd23b
euberdeveloper <euberdeveloper@gmail.com>
2024-01-25 18:30:37 +0100

    feat: add validation pipe
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 
 import { setupSwagger } from './swagger.setup';
 import { setupPrettyJson } from './pretty.setup';
+import { setupValidation } from './validation.setup';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    setupValidation(app);
     setupSwagger(app);
     setupPrettyJson(app);
     await app.listen(3000);
---
+++ src/validation.setup.ts
@@ -0,0 +1,5 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+export function setupValidation(app: INestApplication): void {
+    app.useGlobalPipes(new ValidationPipe());
+}
